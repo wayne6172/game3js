631cc125c5a339ff2e855b65eda862f3f441fabd
jyunming-chen <jyunming.chen@gmail.com>
2017-01-05 20:05:11 +0800

    beautify ccdbox.js

--- js/ccdbox.js
+++ js/ccdbox.js
@@ -6,51 +6,51 @@ typedef struct {
 } CCD_axis;
 */
 
-// assumption a function
-// function fk (theta, joints)
-// that does forward kinematics
-//
-
 var CCD_axis = function(axis, id) 
 {
 	this.jointid = id;
 	this.axis = axis.clone();
 	this.limits = new THREE.Vector2(-1e4,1e4); // default: no limits
-}
+};
 
-function CLAMP(value, lo, hi) {
-  if (value < lo) return lo;
-  if (value > hi) return hi;
-  return value;
-}
+//
+// generic CCD solver
+//
 
-function proj2plane (p, n)
 // p: the vector to be projected
 // n: the normal defining the projection plane (unit vector)
+// clarification: call by reference/pointer or call-by-value
+function proj2plane (p, n)
 {
+  return p.clone().projectOnPlane (n); 
+/*	
 	var nclone = n.clone();
 	var pclone = p.clone();
 
 	nclone.multiplyScalar (p.dot(n));   // (p.n)n;
 	pclone.sub (nclone);
 	return pclone;
+*/	
 }
 
+//var axes = [];
 
 function ik_ccd (target, theta)
 {
 	var end = new THREE.Vector3();
 	var base = new THREE.Vector3();
 	
+	// e.g., njoints = 2;
+	// jointid: 0,0,1
 	var njoints = axes[axes.length-1].jointid + 1;
 	var joints=[];
 	for (var i = 0; i <= njoints; i++) joints[i]=new THREE.Vector3();
 	
 	fk (theta, joints);
-	end.copy (joints[njoints]);
+	end.copy (joints[joints.length-1]]);
 	
 	// convergence
-	var eps = 1e-3, MAXITER = 10;
+	var eps = 1e-1, MAXITER = 20;
 	
 	var t_target = new THREE.Vector3();
 	var t_end = new THREE.Vector3();
@@ -67,6 +67,9 @@ function ik_ccd (target, theta)
 			for (var j = i-1; j >= 0; j--) 
 				axis.applyMatrix4 (new THREE.Matrix4().makeRotationAxis(axes[j].axis, theta[j])); 
 				
+			// after this manipulation,
+			// axis become world coordinate
+			
 			tmpV.subVectors (target, base);	tmpV = proj2plane (tmpV, axis);
 			t_target.copy(tmpV.normalize());
 			
@@ -76,20 +79,21 @@ function ik_ccd (target, theta)
 			var dotV = t_end.dot(t_target);
 			var angle = Math.acos (CLAMP(dotV, -1,1));
 			tmpV.crossVectors (t_end, t_target);
-			var sign =  tmpV.dot(axis) > 0 ? 1: -1;
+			var sign =  (tmpV.dot (new THREE.Vector3(0,1,0)) > 0) ? 1: -1;
 			theta[i] += sign * angle;
 			
 			// joint limit [-2.4, -0.1]
-			theta[i] = CLAMP (theta[i], axes[i].limits.x, axes[i].limits.y)
-      
+			theta[i] = CLAMP (theta[i], axes[i].limits[0], axes[i].limits[1])
+		
 			fk (theta, joints);
-			end.copy(joints[joints.length-1]);  // was a bug ... by setting joints[2]
-
+			end.copy(joints[joints.length-1]);
+			
 			if (end.distanceTo (target) < eps) {
 				return 1;
 			}
 		}
 	}
+	
 	
 	if (iter < MAXITER)
 		return 1;
